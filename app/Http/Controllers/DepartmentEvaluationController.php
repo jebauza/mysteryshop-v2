7b4896e3d7401531d23aed0be92c9842723ecc75
jebauza <ebauza@mobilendo.com>
2020-05-10 23:54:17 +0200

    working

---
+++ app/Http/Controllers/DepartmentEvaluationController.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\DepartmentEvaluation;
+use Illuminate\Http\Request;
+
+class DepartmentEvaluationController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\DepartmentEvaluation  $departmentEvaluation
+     * @return \Illuminate\Http\Response
+     */
+    public function show(DepartmentEvaluation $departmentEvaluation)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\DepartmentEvaluation  $departmentEvaluation
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, DepartmentEvaluation $departmentEvaluation)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\DepartmentEvaluation  $departmentEvaluation
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(DepartmentEvaluation $departmentEvaluation)
+    {
+        //
+    }
+}
