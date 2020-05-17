e87061a26f2e9de1025a23a853b063f1c7ddc92a
jebauza <ebauza@mobilendo.com>
2020-05-17 16:42:25 +0200

    Ready vue en home

--- config/adminlte.php
+++ config/adminlte.php
@@ -193,7 +193,7 @@ return [
     |
     */
 
-    'enabled_laravel_mix' => false,
+    'enabled_laravel_mix' => true,
 
     /*
     |--------------------------------------------------------------------------
